52f93bf481a1c75861e534f2ce2c9daf0f880e13
Enzo Gama <enzo.betabit@gmail.com>
2022-07-23 18:16:55 -0300

    CRUD

--- app.js
+++ app.js
@@ -1,13 +1,64 @@
 const express = require("express");
+const uuid = require('uuid').v4;
 
 const app = express();
 
-app.get("/primeira-rota", (request, response) => {
-    return response.json({
-        data: "Ola isis"
-    });
-});
+app.use(express.json());
+
+const products = [];
+
+// Body => Sempre que eu quiser enviar dados para minha aplicação
+// Params => Vem na URL -> /products/12312312341/123231/teste
+// Query => Parametros opcionais -> /product?id=123&value=Cadeira
  
+app.post("/products", (request, response) => {
+    //nome e preço
+    const {name, price} = request.body;
+
+    const product = {
+        name,
+        price, 
+        id: uuid(),
+    };
+    products.push(product);
+
+    return response.json(product);
+});
+
+app.get("/products", (request, response) => {
+    return response.json(products);
+});
+
+app.get("/products/:id", (request, response) => {
+
+    const { id } = request.params;
+    const product = products.find(product => product.id === id);
+    return response.json(product);
+});
+
+app.put("/products/:id", (request, response) => {
+    const { id } = request.params;
+    const {name, price} = request.body;
+
+    const productIndex = products.findIndex((product) => product.id === id);
+
+    //alterando produto
+    products[productIndex] = {
+        name,
+        price
+    };
+
+    return response.json({message: "Produto alterado com sucesso"});
+});
+
+app.delete("/products/:id", (request, response) => {
+    const { id } = request.params;
+    const productIndex = products.findIndex((product) => product.id === id);
+
+    products.splice(productIndex, 1);
+
+    return response.json({message: "Produto removido com sucesso ! "})
+});
 
 app.listen(4002, () => console.log("Servidore rodando na porta 4002"));
 
