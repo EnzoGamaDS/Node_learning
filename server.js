61e5cf5a086486ed212b9fb47e2629d6e8e69ae4
Enzo Gama <enzo.betabit@gmail.com>
2022-07-23 15:20:54 -0300

    Criando rotas na pedreiragem com Node

--- server.js
+++ server.js
@@ -2,11 +2,17 @@ const http = require("http");
 
 const server = http.createServer((req, res) =>{
     res.writeHead(200, {'Content-Type': 'application/json'});
-    res.end(JSON.stringify({
-        data: 'Enzo Gama2',
-        message: "Olá mundo"
-    }));
-    // res.end("Olá mundo")
+    if (req.url === '/produto') {
+        res.end(JSON.stringify({
+            data: 'Rota de produto',
+        }));
+    }    
+    if (req.url === '/usuario') {
+        res.end(JSON.stringify({
+            data: 'Rota de usuario',
+        }));
+    }
+    res.end("Qualquer outra rota")
 
 });
 
